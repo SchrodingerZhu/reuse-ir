2d8cec4a06e024b1b4da22a693ec786479622209
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-01-08 00:40:58 -0500

    feat: add basic operations

--- Refcnt/src/RefcntOps.cpp
+++ Refcnt/src/RefcntOps.cpp
@@ -1,4 +1,18 @@
+#include <mlir/IR/Diagnostics.h>
+#include <mlir/Support/LogicalResult.h>
+
 #include "Refcnt/RefcntOps.h"
 
 #define GET_OP_CLASSES
 #include "Refcnt/RefcntOps.cpp.inc"
+
+namespace mlir::refcnt {
+LogicalResult DropOp::verify() {
+  if (this->getValue().getType().getPointee() ==
+      this->getToken().getType().getLayout())
+    return success();
+  ::mlir::emitError(getLoc(), "invalid refcnf.drop: pointee type of rc "
+                              "does not match the type of the token.");
+  return failure();
+}
+} // namespace mlir::refcnt
