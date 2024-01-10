4b41e3a8365c4daf3cf26cc1a49a35e70cf3d31a
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2024-01-10 18:09:36 -0500

    misc: qualify attributes in example

--- Tests/UnitTests/GTestLeanBasic.cpp
+++ Tests/UnitTests/GTestLeanBasic.cpp
@@ -98,7 +98,7 @@ SOURCE_PARSE_TEST(GTestLeanBasic, ReverseMap, R"(
         default: ^bb_default,
         0: ^bb_nil,
         1: ^bb_cons
-      ] { type = "List" }
+      ] { lean.type = "List" }
     ^bb_default:
       llvm.unreachable
     ^bb_nil:
@@ -112,7 +112,7 @@ SOURCE_PARSE_TEST(GTestLeanBasic, ReverseMap, R"(
       refcnt.dec %list : !refcnt.rc<!lean.obj>
       refcnt.inc %f : !refcnt.rc<!lean.obj>
       %y = lean.app %f (%hd)
-      %cons = refcnt.new { type = "List" , tag = 1 } : !refcnt.rc<!lean.obj>
+      %cons = refcnt.new { lean.type = "List" , lean.tag = 1 } : !refcnt.rc<!lean.obj>
       lean.set %cons, 0 : index, %y
       lean.set %cons, 1 : index, %updated
       cf.br ^start(%tl, %cons: !refcnt.rc<!lean.obj>, !refcnt.rc<!lean.obj>)
